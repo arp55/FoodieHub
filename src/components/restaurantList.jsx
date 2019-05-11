6654cf8060c4648ff8ec7b2f69dfae05197e635d
Adithya <adithya.rpai55@gmail.com>
2019-05-12 02:35:15 +0530

    Added all files

--- src/components/restaurantList.jsx
+++ src/components/restaurantList.jsx
@@ -91,7 +91,7 @@ class List extends Component {
                               {item.restaurant.user_rating.aggregate_rating}
                             </span>
                             <img
-                              src="http://www.downloadclipart.net/svg/41208-brown-star-star-vector.svg"
+                              src="http://www.downloadclipart.net/thumb/41208-brown-star-star-icon.png"
                               className="ratingImg"
                             />
                           </div>
